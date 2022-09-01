8da140b04eb9359303921a6556cbd2647ec60c1e
simonaspinall9 <simon.aspinall@tote.co.uk>
2022-09-01 23:27:48 +0100

    Initial check in]

---
+++ DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Npgsql;
+using SportReflections.Common.Interfaces;
+
+namespace SportReflections.Common
+{
+    public class DbContext : IDbContext
+    {
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            return new NpgsqlConnection(connectionString);
+        }
+    }
+}
+
---
+++ Interfaces/IDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace SportReflections.Common.Interfaces
+{
+    public interface IDbContext
+    {
+        IDbConnection CreateConnection(string connectionString);
+    }
+}
+
